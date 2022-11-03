fcf51608f139f3aaa4524f2a0b82d794063016f3
Jason <j450nx@gmail.com>
2022-11-03 09:54:24 -0400

    reading .xlsx and .xls files

--- src/test/java/utils/ExcelUtils.java
+++ src/test/java/utils/ExcelUtils.java
@@ -1,18 +1,42 @@
 package utils;
 
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
-	public static void main(String[] args) {
-		getRowCount();
+	
+	// use HSSFWorkbook for .xls files
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	
+	public ExcelUtils(String excelPath, String sheetName) {
+		try {
+			
+			// InputStream = new FileInputStream(excelPath);
+			// workbook = new HSSFWorkbook(new POIFSFileSystem(file));
+			workbook = new XSSFWorkbook(excelPath);
+			sheet = workbook.getSheet(sheetName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void getCellData(int rowNum, int colNum) throws IOException {
+//		String valueAtCell0 = sheet.getRow(1).getCell(0).getStringCellValue();
+//		int valueAtCell2 = (int) sheet.getRow(1).getCell(2).getNumericCellValue();
+		//works for all kinds of data
+		DataFormatter formatter = new DataFormatter();
+		Object valueAtCell = formatter.formatCellValue(sheet.getRow(rowNum).getCell(colNum));
+		System.out.println(valueAtCell);
 	}
 	
 	public static void getRowCount() {
-		
 //		String projDir = System.getProperty("user.dir");
 //		System.out.println(projDir);
-		
-		String excelPath = "./data/TestData.xlsx";
-		XSSFWorkbook workbook = new XSSFWorkbook();
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		System.out.println("Num of rows: " + rowCount);
 	}
 }
